bb0e93144530d5762059b7ba60f93777b9059a66
pablossrudi <pablossrudi@gmail.com>
2025-06-03 21:09:35 -0400

    Agregar integración de sitemap en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://pabloulloadev.site/',
+  integrations: [
+    react(), 
+    sitemap({
+      filter: (route) => {
+        return !route.includes('#');
+      },
+    }),
+  ]
 });
